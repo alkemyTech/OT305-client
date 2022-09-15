efef9ceaaad957c38135d276cab56e89e126a852
laucadi <lauracaicedo182@gmail.com>
2022-09-15 12:22:30 -0500

    login form model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,13 @@
-import { FeaturesModule } from './features/features.module';
-import { CoreModule } from './core/core.module';
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { FeaturesModule } from "./features/features.module";
+import { CoreModule } from "./core/core.module";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CoreModule,
-    FeaturesModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CoreModule, FeaturesModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,6 +2,7 @@ import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { ActivityFormComponent } from "./pages/activities/activity-form/activity-form.component";
 import { LoginFormComponent } from "./pages/auth/login-form/login-form.component";
 import { RegisterFormComponent } from "./pages/auth/register-form/register-form.component";
@@ -10,8 +11,8 @@ import { NewsFormComponent } from "./pages/news/news-form/news-form.component";
 import { SlidesFormComponent } from "./pages/slides/slides-form/slides-form.component";
 import { TestimonialFormComponent } from "./pages/testimonials/testimonial-form/testimonial-form.component";
 import { UserFormComponent } from "./pages/users/user-form/user-form.component";
-import { UsSectionComponent } from './pages/about/us-section/us-section.component';
-import { SobreNosotrosComponent } from './pages/about/us-section/components/sobre-nosotros/sobre-nosotros.component';
+import { UsSectionComponent } from "./pages/about/us-section/us-section.component";
+import { SobreNosotrosComponent } from "./pages/about/us-section/components/sobre-nosotros/sobre-nosotros.component";
 
 @NgModule({
   declarations: [
@@ -35,8 +36,14 @@ import { SobreNosotrosComponent } from './pages/about/us-section/components/sobr
     SlidesFormComponent,
     TestimonialFormComponent,
     UserFormComponent,
-    RouterModule
+    RouterModule,
   ],
-  imports: [CommonModule, AppRoutingModule, RouterModule],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class FeaturesModule {}
