1198ec42272e92e8767f8e065e4085c321ce4cd2
CodeYgriega <oliveryanicelli99@gmail.com>
2022-09-15 14:57:03 -0300

    Validaciones completadas

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,6 +2,9 @@ import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+
+
 import { ActivityFormComponent } from "./pages/activities/activity-form/activity-form.component";
 import { LoginFormComponent } from "./pages/auth/login-form/login-form.component";
 import { RegisterFormComponent } from "./pages/auth/register-form/register-form.component";
@@ -15,6 +18,7 @@ import { SobreNosotrosComponent } from './pages/about/us-section/components/sobr
 import { ContactFormComponent } from './pages/contact/components/contact-form/contact-form.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { ContributesInfoComponent } from './pages/contact/components/contributes-info/contributes-info.component';
+
 
 @NgModule({
   declarations: [
@@ -43,6 +47,12 @@ import { ContributesInfoComponent } from './pages/contact/components/contributes
     UserFormComponent,
     RouterModule
   ],
-  imports: [CommonModule, AppRoutingModule, RouterModule],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
 })
 export class FeaturesModule {}
--- src/app/features/pages/contact/components/contact-form/contact-form.component.ts
+++ src/app/features/pages/contact/components/contact-form/contact-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  
+  constructor(private fb: FormBuilder) {
+
+    this.form = this.fb.group({
+      name: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      phone: ["", [Validators.required, Validators.min(10000000)]],
+      message: ["", Validators.required]
+    })
+
+  }
 
   ngOnInit(): void {
+  }
+
+  sendForm(){
+    
+    if(this.form.valid){
+      console.log(this.form.value); //Acá se realizaría el envío del email
+    }
+    else{
+      alert("¡Por favor rellena los campos!") // Acá se puede agregar tanto alertas personalizadas como otro tipo de mensajes para dar un feedback al usuario
+    }
   }
 
 }
