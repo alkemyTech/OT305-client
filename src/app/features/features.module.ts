1f2747775db800182e7a5b1db5c75531d831f96a
NataliaTodesco <nstodesco@gmail.com>
2022-09-17 18:58:02 -0300

    Conflictos del merge resueltos

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -11,7 +11,6 @@ import { NewsFormComponent } from "./pages/news/news-form/news-form.component";
 import { SlidesFormComponent } from "./pages/slides/slides-form/slides-form.component";
 import { TestimonialFormComponent } from "./pages/testimonials/testimonial-form/testimonial-form.component";
 import { UserFormComponent } from "./pages/users/user-form/user-form.component";
-import { ReactiveFormsModule } from "@angular/forms";
 import { MostrarTitulosComponent } from "../shared/components/mostrar-titulos/mostrar-titulos.component";
 import { UsSectionComponent } from "./pages/about/us-section/us-section.component";
 import { SobreNosotrosComponent } from "./pages/about/us-section/components/sobre-nosotros/sobre-nosotros.component";
