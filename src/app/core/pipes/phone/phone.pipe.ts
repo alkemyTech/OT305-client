7ae33b45725b049f6a7002fefe22524759ecdd5d
Facundo Carollo <65639107+FacundoCarollo@users.noreply.github.com>
2022-09-28 13:19:26 -0300

    redireccion al inicio por medio de un boton

--- src/app/core/pipes/phone/phone.pipe.ts
+++ src/app/core/pipes/phone/phone.pipe.ts
@@ -6,16 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PhonePipe implements PipeTransform {
 
   transform(rawNum : any) {
-  
 
     let newStr = "";
     let i = 0;
 
     for (; i < Math.floor(rawNum.length / 2 ) - 1; i++) {
-      newStr = newStr + rawNum.substr(i * 2, 2) + "-";
+      newStr =   (newStr)  +  rawNum.substr(i * 2, 2 ) + "-" ;
     }
 
-    return newStr + rawNum.substr(i * 2);
+    return newStr + rawNum.substr(i * 2) ;
   }
 
 }
