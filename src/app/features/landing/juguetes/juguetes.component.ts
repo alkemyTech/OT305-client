bc7fd25bc21e027abca647246a31c28ef09c186e
NataliaTodesco <nstodesco@gmail.com>
2022-09-29 11:51:00 -0300

    Componente de la campaña juguetes

---
+++ src/app/features/landing/juguetes/juguetes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juguetes',
+  templateUrl: './juguetes.component.html',
+  styleUrls: ['./juguetes.component.scss']
+})
+export class JuguetesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
