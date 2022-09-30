390f6c9b8538855099dbe25240b80606a3c8100b
AlejoGalvez <alejo.galvez0@gmail.com>
2022-09-30 18:28:21 -0300

    servicio contacts

---
+++ src/app/core/models/contacts.model.ts
@@ -0,0 +1,11 @@
+export class Contacts {
+    id?:         number;
+    name?:       string;
+    email?:      string;
+    phone?:      string;
+    message?:    string;
+    deleted_at?: Date;
+    created_at?: Date;
+    updated_at?: Date;
+    group_id?:   number;
+}
