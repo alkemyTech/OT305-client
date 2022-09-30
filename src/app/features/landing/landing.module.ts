9eeeaacf0f7105e59fe7d1eda7e8a9faab8bceac
laucadi <lauracaicedo182@gmail.com>
2022-09-29 22:25:42 -0500

    slider campana escolar

--- src/app/features/landing/landing.module.ts
+++ src/app/features/landing/landing.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { LandingRoutingModule } from './landing-routing.module';
 import { JuguetesComponent } from './juguetes/juguetes.component';
 import { EscolarComponent } from './escolar/escolar.component';
+import { SliderComponent } from './slider/slider.component';
 
 
 
@@ -14,7 +15,8 @@ import { EscolarComponent } from './escolar/escolar.component';
     LandingComponent,
     FooterComponent,
     JuguetesComponent,
-    EscolarComponent
+    EscolarComponent,
+    SliderComponent
   ],
   imports: [
     CommonModule,
