ef61aaff8dd3223fa5e38579b1720bd836993fb3
NataliaTodesco <nstodesco@gmail.com>
2022-09-30 13:24:19 -0300

    Creación e implementación de variables de entorno de testimonios y servicio de testimonios

--- src/app/features/pages/testimonials/testimonial-form/testimonial-form.component.ts
+++ src/app/features/pages/testimonials/testimonial-form/testimonial-form.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnDestroy } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Subject } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 import { HttpService } from "src/app/core/services/http.service";
+import { TestimonioService } from "src/app/core/services/testimonials/testimonio.service";
 import Swal from "sweetalert2";
 
 @Component({
@@ -8,13 +11,17 @@ import Swal from "sweetalert2";
   templateUrl: "./testimonial-form.component.html",
   styleUrls: ["./testimonial-form.component.scss"],
 })
-export class TestimonialFormComponent {
+export class TestimonialFormComponent implements OnDestroy{
+  private desub$ = new Subject<void>();
   @Input() testimonio: any;
   accion: string;
   form: FormGroup;
   foto: any;
 
-  constructor(private fb: FormBuilder, private http: HttpService) {
+  constructor(
+    private fb: FormBuilder,
+    private testimonioService: TestimonioService
+  ) {
     this.form = this.fb.group({
       image: [null, Validators.required],
       name: ["", [Validators.required, Validators.minLength(4)]],
@@ -45,104 +52,106 @@ export class TestimonialFormComponent {
     }
   }
 
+  onSubmit() {
+    if (this.accion === "Agregar") this.crearTestimonio();
+    else this.actualizarTestimonio();
+  }
+
   crearTestimonio() {
-    if (this.accion === "Agregar") {
-      // petición POST al endpoint de creación (/testimonials/create)
+    // petición POST al endpoint de creación (/testimonials/create)
+    let data = {
+      id: 0,
+      name: this.form.value.name,
+      image: this.form.value.image,
+      description: this.form.value.description,
+      created_at: new Date(),
+      updated_at: null,
+      deleted_at: null,
+    };
 
-      this.http
-        .post(
-          "https://ongapi.alkemy.org/api/testimonials",
-          {
-            id: 0,
-            name: this.form.value.name,
-            image: this.form.value.image,
-            description: this.form.value.description,
-            created_at: new Date(),
-            updated_at: null,
-            deleted_at: null,
-          },
-          false
-        )
-        .subscribe(
-          () => {
-            Swal.fire(
-              "Testimonio Agregado!",
-              "El testimonio fue agregado éxitosamente",
-              "success"
-            );
-            this.foto = null;
-            this.form.controls["image"].setValue(null);
-            this.form.controls["name"].setValue("");
-            this.form.controls["description"].setValue("");
-          },
-          (error) => {
-            Swal.fire(
-              "El testimonio no pudo ser Agregado",
-              error.messagge,
-              "error"
-            );
-          }
+    this.testimonioService.setTestimonio(data).pipe(takeUntil(this.desub$))
+    .subscribe(
+      () => {
+        Swal.fire(
+          "Testimonio Agregado!",
+          "El testimonio fue agregado éxitosamente",
+          "success"
         );
-    } else {
-      //petición PATCH al endpoint de actualización del server (/testimonials/:id).
+        this.foto = null;
+        this.form.controls["image"].setValue(null);
+        this.form.controls["name"].setValue("");
+        this.form.controls["description"].setValue("");
+      },
+      (error) => {
+        Swal.fire(
+          "El testimonio no pudo ser Agregado",
+          error.messagge,
+          "error"
+        );
+      }
+    );
+  }
 
-      if (this.form.value.image === null) {
-        this.http
-          .patch(
-            `https://ongapi.alkemy.org/api/testimonials/${this.testimonio.id}`,
-            {
-              name: this.form.value.name,
-              description: this.form.value.description,
-              image: this.form.value.image,
-              updated_at: new Date(),
-            },
-            false
-          )
-          .subscribe(
-            (data) => {
-              console.log(data);
-              Swal.fire(
-                "Testimonio Editado!",
-                "El testimonio fue editado éxitosamente",
-                "success"
-              );
-            },
-            (error) => {
-              Swal.fire(
-                "El testimonio no pudo ser Editado",
-                error.messagge,
-                "error"
-              );
-            }
+  actualizarTestimonio() {
+    //petición PATCH al endpoint de actualización del server (/testimonials/:id).
+
+    if (this.form.value.image === null) {
+      let data = {
+        name: this.form.value.name,
+        description: this.form.value.description,
+        image: this.form.value.image,
+        updated_at: new Date(),
+      };
+
+      this.testimonioService.updateTestimonio(data).pipe(takeUntil(this.desub$))
+      .subscribe(
+        (data) => {
+          console.log(data);
+          Swal.fire(
+            "Testimonio Editado!",
+            "El testimonio fue editado éxitosamente",
+            "success"
           );
-      } else
-        this.http
-          .patch(
-            `https://ongapi.alkemy.org/api/testimonials/${this.testimonio.id}`,
-            {
-              name: this.form.value.name,
-              description: this.form.value.description,
-              updated_at: new Date(),
-            },
-            false
-          )
-          .subscribe(
-            (data) => {
-              console.log(data);
-              Swal.fire(
-                "Testimonio Editado!",
-                "El testimonio fue editado éxitosamente",
-                "success"
-              );
-            },
-            (error) => {
-              Swal.fire(
-                "El testimonio no pudo ser Editado",
-                error.messagge,
-                "error"
-              );
-            }
+        },
+        (error) => {
+          Swal.fire(
+            "El testimonio no pudo ser Editado",
+            error.messagge,
+            "error"
           );
+        }
+      );
+    } else {
+      let data = {
+        name: this.form.value.name,
+        description: this.form.value.description,
+        updated_at: new Date(),
+      };
+
+      this.testimonioService.updateTestimonio(data).pipe(takeUntil(this.desub$))
+      .subscribe(
+        (data) => {
+          console.log(data);
+          Swal.fire(
+            "Testimonio Editado!",
+            "El testimonio fue editado éxitosamente",
+            "success"
+          );
+        },
+        (error) => {
+          Swal.fire(
+            "El testimonio no pudo ser Editado",
+            error.messagge,
+            "error"
+          );
+        }
+      );
     }
   }
+
+  ngOnDestroy(): void {
+    this.desub$.next();
+    this.desub$.complete();
+  }
+
 }
