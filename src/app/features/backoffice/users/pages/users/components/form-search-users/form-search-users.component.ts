2582f582c855b11b30574888711a25d62080a36b
CodeYgriega <oliveryanicelli99@gmail.com>
2022-10-03 22:16:46 -0300

    se agrego un else en setRol

--- src/app/features/backoffice/users/pages/users/components/form-search-users/form-search-users.component.ts
+++ src/app/features/backoffice/users/pages/users/components/form-search-users/form-search-users.component.ts
@@ -69,6 +69,9 @@ export class FormSearchUsersComponent implements OnInit, OnDestroy {
       this.rolSolicitado = rol;
       this.subject$.next(rol);
     }
+    else{
+      this.rolSolicitado = rol;
+    }
   }
 
 }
