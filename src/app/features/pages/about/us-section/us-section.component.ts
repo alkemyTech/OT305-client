805a5248c83c1ac2964b8203c068c29811003cac
NataliaTodesco <nstodesco@gmail.com>
2022-10-06 17:24:27 -0300

    Integracion de redes en nosotros

--- src/app/features/pages/about/us-section/us-section.component.ts
+++ src/app/features/pages/about/us-section/us-section.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { OrganizacionService } from 'src/app/core/services/organization/organizacion.service';
+import { takeUntil } from "rxjs/operators";
 @Component({
   selector: 'app-us-section',
   templateUrl: './us-section.component.html',
   styleUrls: ['./us-section.component.scss']
 })
-export class UsSectionComponent implements OnInit {
+export class UsSectionComponent implements OnInit, OnDestroy {
+  texto: string = '';
+  private desub$ = new Subject<void>();
 
-  constructor() { }
+  constructor(private organizacionService: OrganizacionService) { }
 
   ngOnInit(): void {
+    this.organizacionService.getOrganizacion().pipe(takeUntil(this.desub$))
+    .subscribe(
+      (res) => {
+        let {data} : any = res;
+        this.texto = data.long_description;
+      },
+      (error) => console.log(error.message)
+    );
   }
 
+  ngOnDestroy(): void {
+    this.desub$.next();
+    this.desub$.complete();
+  }
 }
