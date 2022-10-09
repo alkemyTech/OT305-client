0b3a710f4102522c4747b1697fbba381f954f0ea
laucadi <lauracaicedo182@gmail.com>
2022-10-08 20:59:27 -0500

    logica form contact

--- src/app/features/pages/contact/components/contact-form/contact-form.component.ts
+++ src/app/features/pages/contact/components/contact-form/contact-form.component.ts
@@ -1,37 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { MatDialog } from "@angular/material/dialog";
+import { Contacts } from "src/app/core/models/contacts.model";
+import { ContactsService } from "src/app/core/services/contacts/contacts.service";
+import { AlertasComponent } from "src/app/shared/components/alertas/alertas.component";
 
 @Component({
-  selector: 'app-contact-form',
-  templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.scss']
+  selector: "app-contact-form",
+  templateUrl: "./contact-form.component.html",
+  styleUrls: ["./contact-form.component.scss"],
 })
 export class ContactFormComponent implements OnInit {
+  form!: FormGroup;
+  contactModel: Contacts = new Contacts();
 
-  form: FormGroup;
-  
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private contactService: ContactsService,
+    public dialog: MatDialog
+  ) {}
 
+  ngOnInit(): void {
+    this.campos();
+  }
+  campos() {
     this.form = this.fb.group({
       name: ["", Validators.required],
       email: ["", [Validators.required, Validators.email]],
-      phone: ["", [Validators.required, Validators.min(10000000)]],
-      message: ["", Validators.required]
-    })
-
+      message: ["", Validators.required],
+    });
   }
 
-  ngOnInit(): void {
-  }
-
-  sendForm(){
-    
-    if(this.form.valid){
-      console.log(this.form.value); //Acá se realizaría el envío del email
+  sendForm() {
+    this.contactModel.name = this.form.value.name;
+    this.contactModel.email = this.form.value.email;
+    this.contactModel.message = this.form.value.message;
+    if (this.form.invalid) {
+      this.openDialog("error!", "Por favor rellena todos los campos");
+    } else {
+      this.contactService.setContact(this.contactModel).subscribe(
+        (res) => {
+          console.log(res);
+          this.openDialog(
+            "Mensaje enviado exitosamente!",
+            "Pronto nos pondremos en contacto contigo"
+          );
+          this.form.reset();
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
     }
-    else{
-      alert("¡Por favor rellena los campos!") // Acá se puede agregar tanto alertas personalizadas como otro tipo de mensajes para dar un feedback al usuario
-    }
   }
-
+  openDialog(titulo: string, mensaje: string): void {
+    const dialogRef = this.dialog.open(AlertasComponent, {
+      width: "350px",
+      data: {
+        cancelText: "Cerrar",
+        confirmText: "Ok",
+        message: mensaje,
+        title: titulo,
+      },
+    });
+  }
 }
