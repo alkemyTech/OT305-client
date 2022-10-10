00a4005d89a7b9f63cb62a3af483dc62bdba39d3
Laura Caicedo <64761438+laucadi@users.noreply.github.com>
2022-10-10 09:46:38 -0500

    Ft/ot 305/errores home (#95)
    * cambios en inicio y logica de slides
    
    * logica slides y refactorizacion del home

--- src/app/features/pages/home/slider/slider.component.ts
+++ src/app/features/pages/home/slider/slider.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { Slide } from "src/app/core/models/slide.model";
+import { SlidesService } from "src/app/core/services/slides/slides.service";
+import { AlertasComponent } from "src/app/shared/components/alertas/alertas.component";
 
 @Component({
   selector: "app-slider",
@@ -6,37 +10,27 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./slider.component.scss"],
 })
 export class SliderComponent implements OnInit {
-  //cambiar despues por los endopoints api sliders
-  public data = ([] = [
-    {
-      name: "Juntos somos más",
-      description: "<p>Sé parte de nuestra comunidad y cambiemos el mundo.</p>",
-      image: "http://ongapi.alkemy.org/storage/2sjocle1Nz.png",
-    },
-    {
-      name: "Los niños son el futuro.",
-      description: "Acompañemos a nuestros niños a construir un mejor futuro.",
-      image: "http://ongapi.alkemy.org/storage/HjvFnkQSgV.png",
-    },
-    {
-      name: "Exploremos nuevos horizontes",
-      description: "Somos impulsores de nuevas experiencias.",
-      image: "http://ongapi.alkemy.org/storage/AdEulUIBmO.png",
-    },
-    {
-      name: "Cuidamos de vos.",
-      description:
-        "Queremos estar junto a vos, nuestra comunidad esta para apoyarte.",
-      image: "http://ongapi.alkemy.org/storage/bvaTynsmk9.png",
-    },
-    {
-      name: "Compartí",
-      description:
-        "Compartí con nosotros, queremos que seas parte de nuestra comunidad.",
-      image: "http://ongapi.alkemy.org/storage/4DfanYm37r.png",
-    },
-  ]);
-  constructor() {}
+  slides: Slide[] = [];
+  slides$!: Observable<Slide[]>;
+  dialog: any;
 
-  ngOnInit(): void {}
+  constructor(private slideService: SlidesService) {}
+
+  ngOnInit(): void {
+    this.slides$ = this.slideService.getSlide();
+    this.slides$.subscribe((err) => {
+      this.openDialog("error!", "Por favor rellena todos los campos");
+    });
+  }
+  openDialog(titulo: string, mensaje: string): void {
+    const dialogRef = this.dialog.open(AlertasComponent, {
+      width: "350px",
+      data: {
+        cancelText: "Cerrar",
+        confirmText: "Ok",
+        message: mensaje,
+        title: titulo,
+      },
+    });
+  }
 }
