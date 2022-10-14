863f63a25b35feb13ef74ee892c323700355e279
ealbarracindev <ealbarracindev@gmail.com>
2022-10-14 19:05:27 -0300

    agregado en la configuracion del tsconfig

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+//import packageJson from '../../package.json';
+const packageJson = require('../../package.json');
 export const environment = {
   production: false,
   apiUrl: "https://ongapi.alkemy.org/api",
@@ -14,6 +15,16 @@ export const environment = {
   categories: "/categories",
   slides: "/slides",
   users: "/users",
+  /**
+   * Versiones por numero: 
+   *  El primero (X) se le conoce como versión mayor y nos indica la versión principal del software. Ejemplo: 1.0.0, 3.0.0
+      El segundo (Y) se le conoce como versión menor y nos indica nuevas funcionalidades. Ejemplo: 1.2.0, 3.3.0
+      El tercero (Z) se le conoce como revisión y nos indica que se hizo una revisión del código por algun fallo. Ejemplo: 1.2.2, 3.3.4
+  */
+ //version: '1.0.0'
+  versions: {
+    app: packageJson.version,
+  }
 };
 
 /*
