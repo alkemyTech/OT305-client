40bb0f1c7d2d59049760042baae0d3510793b9e9
Facundo Carollo <65639107+FacundoCarollo@users.noreply.github.com>
2022-10-18 15:49:56 -0300

    seccion ultimo eveento novedades

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -9,11 +9,15 @@ export class CardComponent implements OnInit {
   @Input() title: any;
   @Input() img!: any;
   @Input() description!: any;
+  @Input() created_at!: any;
+  @Input() updated_at!: any;
 
   constructor() {
     this.title = "";
     this.img = "../../../../assets/placeholder-image.png";
     this.description = "";
+    this.created_at = ""
+    this.updated_at = ""
   }
 
   ngOnInit(): void {}
