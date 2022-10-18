8e8e718f6baf0b960b25d00ed03058b1deb1a831
Laura Caicedo <64761438+laucadi@users.noreply.github.com>
2022-10-18 18:56:04 -0500

    Ft/ot305 25/formulario miembros test (#121)
    * creacion de test y documentacion de formulario miembros
    
    * creacion de test y documentacion de formulario miembros

--- src/app/features/backoffice/members/components/form-crear-editar-miembros/form-crear-editar-miembros.component.spec.ts
+++ src/app/features/backoffice/members/components/form-crear-editar-miembros/form-crear-editar-miembros.component.spec.ts
@@ -1,16 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from "@angular/core/testing";
+import { FormBuilder } from "@angular/forms";
+import { By } from "@angular/platform-browser";
 
-import { FormCrearEditarMiembrosComponent } from './form-crear-editar-miembros.component';
+import { FormCrearEditarMiembrosComponent } from "./form-crear-editar-miembros.component";
 
-describe('FormCrearEditarMiembrosComponent', () => {
+describe("FormCrearEditarMiembrosComponent", () => {
   let component: FormCrearEditarMiembrosComponent;
   let fixture: ComponentFixture<FormCrearEditarMiembrosComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormCrearEditarMiembrosComponent ]
-    })
-    .compileComponents();
+      declarations: [FormCrearEditarMiembrosComponent],
+      providers: [FormBuilder],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -18,8 +20,36 @@ describe('FormCrearEditarMiembrosComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
+  //Creacion correcta del componente (QUE EXISTA)
+  it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  //Se reliza la verificacion de un nuevo miembro, adicional que todos los campos se llenen correctamente
+  it("should create new member", fakeAsync(() => {
+    const testMember = {
+      name: "nombre",
+      image: "imagen",
+      description: "description",
+      facebookUrl: "facebook",
+      linkedinUrl: "linkedin",
+    };
+    component.form.controls["name"].setValue(testMember.name);
+    component.form.controls["image"].setValue(testMember.image);
+    component.form.controls["description"].setValue(testMember.description);
+    component.form.controls["facebookUrl"].setValue(testMember.facebookUrl);
+    component.form.controls["linkedinUrl"].setValue(testMember.linkedinUrl);
+
+    expect(component.form.value).toBeTruthy(testMember);
+  }));
+  // Se realiza la verificacion: si no se llenan todos los campos, o el formulario es invalido
+  it("invalid form error", () => {
+    const fixture = TestBed.createComponent(FormCrearEditarMiembrosComponent);
+    const member = fixture.componentInstance;
+    fixture.detectChanges();
+
+    const name = component.form.controls["name"];
+    name.setValue("juan");
+    expect(member.form.invalid).toBeTrue();
   });
 });
