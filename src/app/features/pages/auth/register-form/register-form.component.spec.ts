29e0d49e9967862d29d4be737f3e018fb02311e4
CodeYgriega <oliveryanicelli99@gmail.com>
2022-10-20 20:37:38 -0300

    testing module configurado correctamente

--- src/app/features/pages/auth/register-form/register-form.component.spec.ts
+++ src/app/features/pages/auth/register-form/register-form.component.spec.ts
@@ -1,14 +1,37 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { PrivateApiService } from 'src/app/core/services/privateApi/private-api.service';
 
 import { RegisterFormComponent } from './register-form.component';
 
 describe('RegisterFormComponent', () => {
   let component: RegisterFormComponent;
   let fixture: ComponentFixture<RegisterFormComponent>;
+  let service: PrivateApiService;
+  let store: MockStore;
+
+  const initialState = {
+    registerSuccess: false
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterFormComponent ]
+      declarations: [ RegisterFormComponent ],
+      imports: [
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule,
+        MatDialogModule
+      ],
+      providers: [
+        PrivateApiService,
+        provideMockStore({ initialState })
+      ]
     })
     .compileComponents();
   });
@@ -16,10 +39,19 @@ describe('RegisterFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RegisterFormComponent);
     component = fixture.componentInstance;
+    service = fixture.debugElement.injector.get(PrivateApiService);
+    store = TestBed.inject(MockStore);
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('', () => {
+    
+  });
+
+
 });
