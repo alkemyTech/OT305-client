5bfd9d1ec698d4255bcf75813721f3e469149e51
CodeYgriega <oliveryanicelli99@gmail.com>
2022-10-20 23:43:05 -0300

    tests para validacion de formulario

--- src/app/features/pages/auth/register-form/register-form.component.spec.ts
+++ src/app/features/pages/auth/register-form/register-form.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatDialogModule } from '@angular/material/dialog';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { PrivateApiService } from 'src/app/core/services/privateApi/private-api.service';
@@ -49,9 +50,26 @@ describe('RegisterFormComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('', () => {
-    
+  it('debe ser invalido el formulario al no rellenarse todos los campos', () => {
+    const form = component.miFormulario;
+
+    form.get("name")?.setValue("Prueba");
+    form.get("email")?.setValue("pruebapararegistro@gmail.com");
+    form.get("password")?.setValue("prueba123!");
+    form.get("password2")?.setValue(null);
+
+    expect(form.valid).toBeFalsy();
   });
 
+  it('debe ser valido el formulario al rellenarse todos los campos', () => {
+    const form = component.miFormulario;
+
+    form.get("name")?.setValue("Prueba");
+    form.get("email")?.setValue("pruebapararegistro@gmail.com");
+    form.get("password")?.setValue("prueba123!");
+    form.get("password2")?.setValue("prueba123!");
+
+    expect(form.valid).toBeTruthy();
+  });
 
 });
